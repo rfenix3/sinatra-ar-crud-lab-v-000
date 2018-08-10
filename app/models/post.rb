d4156ffcb94dd21d2d70db7581943f24cf103952
Richard Fenix <richard.fenix@gmail.com>
2018-08-10 06:43:03 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,21 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+  # @@blogs = []
+  # attr_accessor :name, :content
+  #
+  # def initialize(blog)
+  #   @name = blog[:name]
+  #   @content = blog[:content]
+  # end
+  #
+  # def self.create(blog)
+  #   self.new(blog)
+  #   @@blogs << self
+  # end
+  #
+  # def self.all
+  #   @@blogs
+  # end
+
 end
